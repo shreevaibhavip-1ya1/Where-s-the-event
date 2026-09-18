ef8e720bd0de7c4671214befe37d99281b45f8d8
shreevaibhavip-1ya1 <shreevaibhavip@gmail.com>
2026-09-18 16:42:44 +0530

    this project is about virus detecter

--- vaibhavi.py
+++ vaibhavi.py
@@ -1,28 +1,20 @@
 # Import necessary libraries
 from tkinter import *
+from tkinter import messagebox
 
-# Create window
-window = Tk()
-window.title("Event Handler")
-window.geometry("100x100")
+# Setup Tkinter Window
+root = Tk()
+root.geometry("200x200")
 
-# Event Handler for Keypress
-def handle_keypress(event):
-    """Print the character associated to the key pressed"""
-    print(event.char)
+# Function for Displaying Warning Message
+# This will be called once the button is clicked
+# messagebox.showwarning("Window Name", "Text to be displayed")
+def msg():
+	messagebox.showwarning("Alert", "Stop! Virus Found.")
 
-# Bind keypress event to handle_keypress()
-window.bind("<Key>", handle_keypress)
+# Adding Button Widget to Window
+button = Button(root, text="Scan for Virus", command=msg)
+button.place(x=40, y=80)
 
-# Event handler for button click
-def handle_click(event):
-    print("\nThe button was clicked!")
-
-button = Button(text="Click me!")
-button.pack()
-
-# Bind click event to handle_click()
-button.bind("<Button-1>", handle_click)
-
-# Start the GUI event loop
-window.mainloop()
+# Entering main event loop
+root.mainloop()
